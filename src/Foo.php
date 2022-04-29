d813ae2e1c8b35cd766bf4bd6d0f4da254a320ce
Martin Herndl <martin.herndl@icis.com>
2022-04-29 09:11:25 +0200

    Add error info

--- src/Foo.php
+++ src/Foo.php
@@ -9,6 +9,8 @@ final class Foo
 
     public function doFoo(?string $foo): void
     {
+        // The next line needs to error with
+        // Parameter #1 $text of function esc_html expects string, string|null given.
         echo esc_html($foo);
     }
 
